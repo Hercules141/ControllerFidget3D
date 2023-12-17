9275eab266774551dab9bd2b219659a8d8a1053b
Hercules141 <vincentcoby@outlook.de>
2023-12-17 23:17:09 +0100

    Add Controller Support, Physics Library, general working prototype for controller usage

--- ControllerFidget3D/MainWindow.xaml.cs
+++ ControllerFidget3D/MainWindow.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Channels;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Media3D;
+using Timer = System.Timers.Timer;
 
 namespace ControllerFidget3D
 {
@@ -14,18 +18,30 @@ namespace ControllerFidget3D
         private Point _lastMousePosition;
         private bool _mousePressed;
         private PerspectiveCamera _camera;
+        private XBoxController controller = new XBoxController();
 
+        private Timer controllerTimer;
+        private int pollCounter;
+        
         public MainWindow()
         {
             InitializeComponent();
             
+           
+            
             _camera = new PerspectiveCamera
             {
                 Position = new Point3D(5, 5, 5),
-                LookDirection = new Vector3D(-5, -5, -4),
-                UpDirection = new Vector3D(0, 1, 0)
+                LookDirection = new Vector3D(-5, -5, -5),
+                UpDirection = new Vector3D(0, 0, 1)
             };
             MyViewPort3D.Camera = _camera;
+            
+            // set Controller Event Handler
+            controllerTimer = new Timer(16);
+            controllerTimer.Elapsed += (sender, args) => OnControllerPoll();
+            controllerTimer.Start();
+            
         }
 
         private void Viewport3D_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -65,7 +81,47 @@ namespace ControllerFidget3D
 
             }
 
+            // controllerConnectedTextBlock.Text = controller.rightThumb.X.ToString();
+            
             _lastMousePosition = currentPosition;
+        }
+        
+        // controller event handler
+        public void OnControllerPoll()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                controller.Update();
+                updateControllerInfoDisplay();
+                
+                // update camera position
+                _camera.Position =  Point3D.Add(
+                    _camera.Position,
+                    new Vector3D(controller.leftThumb.X, controller.leftThumb.Y, controller.leftTrigger/255 - controller.rightTrigger/255)
+                );
+                // update camera angle, actually vector..
+                _camera.LookDirection += new Vector3D(controller.rightThumb.X/10*-1, controller.rightThumb.Y/10*-1, 0);
+                
+            });
+        }
+
+        
+        
+        
+        // helper functions
+        private void updateControllerInfoDisplay()
+        {
+            controllerConnectedTextBlock.Text = controller.connected.ToString();
+            controllerInfoTextBlock.Text = "Controller Info:    "
+                                           + "\nLeft Thumb X:   " + controller.leftThumb.X
+                                           + "\nLeft Thumb Y:   " + controller.leftThumb.Y
+                                           + "\nRight Thumb X:  " + controller.rightThumb.X
+                                           + "\nRight Thumb Y:  " + controller.rightThumb.Y
+
+                                           + "\nL Trigger:      " + controller.leftTrigger
+                                           + "\nR Trigger:      " + controller.rightTrigger
+
+                                           + "\ndead thing:     " + controller.deadZone;
         }
     }
 }
---
+++ ControllerFidget3D/XBoxController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using SharpDX.XInput;
+
+namespace ControllerFidget3D
+{
+    public class XBoxController
+    {
+        private Controller controller;
+        private Gamepad gamepad;
+        public bool connected = false;
+        public int deadZone = 2500;
+        public Point leftThumb, rightThumb = new Point(0, 0);
+        public float leftTrigger, rightTrigger;
+
+        public XBoxController()
+        {
+            controller = new Controller(UserIndex.One);
+            Console.WriteLine("Controller State: " + controller.IsConnected);
+            connected = controller.IsConnected;
+        }
+
+        public void Update()
+        {
+            if (!connected)
+                return;
+
+            gamepad = controller.GetState().Gamepad;
+
+            leftThumb.X = (Math.Abs((float)gamepad.LeftThumbX) < deadZone) ? 0 : (float)gamepad.LeftThumbX / short.MaxValue;
+            leftThumb.Y = (Math.Abs((float)gamepad.LeftThumbY) < deadZone) ? 0 : (float)gamepad.LeftThumbY / short.MaxValue;
+            rightThumb.X = (Math.Abs((float)gamepad.RightThumbX) < deadZone) ? 0 : (float)gamepad.RightThumbX / short.MaxValue;
+            rightThumb.Y = (Math.Abs((float)gamepad.RightThumbY) < deadZone) ? 0 : (float)gamepad.RightThumbY / short.MaxValue;
+
+            leftTrigger = gamepad.LeftTrigger;
+            rightTrigger = gamepad.RightTrigger;
+        }
+    }
+}
